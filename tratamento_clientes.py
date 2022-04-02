a6eead97d806953a87309eb9e5b31b5bbd1c9feb
gersontolentinoa <101021871+gersontolentinoa@users.noreply.github.com>
2022-04-02 09:31:20 -0300

    Update tratamento_clientes.py

--- tratamento_clientes.py
+++ tratamento_clientes.py
@@ -2,14 +2,24 @@
 import pandas as pd
 import base64
 import os
-from functools import reduce
 #--------------------------------------------------------------------------------------------------------------#
-#os.remove('TB_BASE_CLINTES.csv')    
-def concatenar(nome_arquivo: str):
+def concatenar_transformar_base64(nome_arquivo: str):
     df = pd.read_csv(nome_arquivo, sep ='\t')
-    df['base'] = df['client_ID']+":"+df['secret']
-    return df
-#concatenar('Lista_Clientes_fake.csv')
+    df1= list(df['client_ID']+":"+df['secret'])
+    df['base'] = [str(base64.b64encode(x.encode('utf-8','string')))[2:-1] for x in df1]
+
+    return (df)
+def gerar_arquivo(dataframe: pd.DataFrame):
+    dataframe.to_csv('TB_BASE_CLINTES.csv')
+    print("TB_BASE_CLINTES.csv gerado corretamente")
+    print(dataframe.head())
+    pass
+gerar_arquivo(concatenar_transformar_base64('C:\\Users\\DELL\\Documents\\Projeto_Safra\\SafraHackathon2022-Time16\\Lista_Clientes_fake.csv'))
+
+
+
+
+
 
 
 
