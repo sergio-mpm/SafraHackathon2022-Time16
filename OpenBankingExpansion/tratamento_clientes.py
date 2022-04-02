ab230080860ca73665b85f1c3d41468ef001c7d7
Sergio Gustavo M P Moreira <sgmpm9@gmail.com>
2022-04-02 02:28:15 -0300

    Update

--- OpenBankingExpansion/tratamento_clientes.py
+++ OpenBankingExpansion/tratamento_clientes.py
@@ -8,8 +8,11 @@ from functools import reduce
 def concatenar(nome_arquivo: str):
     df = pd.read_csv(nome_arquivo, sep ='\t')
     df['base'] = df['client_ID']+":"+df['secret']
+    df['base'] = [base64.b64encode(x.encode('utf-8','string')) for x in df['base']]
     return df
 #concatenar('Lista_Clientes_fake.csv')
 
+def convertbase64(lista_base: list):
+    return (base64.b64encode(lista_base), lista_base)
 
-
+print(list(concatenar('D:\\Trabalho\\Projects\\SafraHackathon2022-Time16\\Lista_Clientes_fake.csv')))
